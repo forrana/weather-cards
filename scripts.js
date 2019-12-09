c38cf1f8985f52c88dbec15e14832994f2bd9d97
Aliaksei Kuchyn <forrana@gmail.com>
2019-12-09 22:55:35 +0200

    Fix winter months description

--- scripts.js
+++ scripts.js
@@ -19,16 +19,16 @@ var app = new Vue({
     questions: [
       ["Tammikuu", "January. Tammikuussa on kylmä. On pakkasta. Sataa lunta ja maassa on lunta.", "", ["chapter 1"]],
       ["Helmikuu",  "February. Helmikuussa on kylmä. On pakkasta. Sataa lunta ja maassa on lunta.", "", ["chapter 1"]],
-      ["Maaliskuu", "March. Maaliskuussa on kelmä, sataa joskus räntää", "", ["chapter 1"]],
+      ["Maaliskuu", "March. Maaliskuussa on kylmä, sataa joskus räntää", "", ["chapter 1"]],
       ["Huhtikuu",  "April. Huhtikuussa sata joskus räntää ja on viilea ja on sumuista.", "", ["chapter 1"]],
       ["Toukokuu", "May. Toukokuussa on jo aika lämmin", "", ["chapter 1"]],
       ["Kesäkuu",  "June. Kesäkuussa aurinko paistaa ja on lämmin.", "", ["chapter 1"]],
       ["Heinäkuu", "July. Heinäkuussa aurinko paistaa ja on lämmin.", "", ["chapter 1"]],
       ["Elokuu",  "August. Elokuussa aurinko paistaa ja on lämmin.", "", ["chapter 1"]],
       ["Syyskuu", "September. Syyskuussa on viilea ja on tuulista.", "", ["chapter 1"]],
-      ["Lokakuu",  "October. ", "Lokakuussa on harmaa ja sataa paljon vettä tai rantää", ["chapter 1"]],
-      ["Marraskuu", "November. ", "Marraskuussa on harmaa ja sataa paljon vettä tai rantää", ["chapter 1"]],
-      ["Joulukuu",  "December. ", "Joulukuussa on viileä, sataa räntää ja lunta, joskus maassa on lunta", ["chapter 1"]],
+      ["Lokakuu",  "October. Lokakuussa on harmaa ja sataa paljon vettä tai rantää.", "", ["chapter 1"]],
+      ["Marraskuu", "November. Marraskuussa on harmaa ja sataa paljon vettä tai rantää.", "", ["chapter 1"]],
+      ["Joulukuu",  "December. Joulukuussa on viileä, sataa räntää ja lunta, joskus maassa on lunta.", "", ["chapter 1"]],
     ],
     current: 0,
     isAnswerShown: false,
@@ -51,7 +51,7 @@ var app = new Vue({
       pronounceTheQuestion(this.includedQuestion[this.current][0])
     },
     prevQuestion: function () {
-      this.current = this.current - 1 > 0 ? this.current - 1 : this.includedQuestion.length - 1
+      this.current = this.current - 1 >= 0 ? this.current - 1 : this.includedQuestion.length - 1
       pronounceTheQuestion(this.includedQuestion[this.current][0])
     },
     randomQuestoin: function () {
